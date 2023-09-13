76bb2692a5af5a808d6e6fa0b3046f8b7a3ca59c
silviapetrino <petrino.silvia@gmail.com>
2023-09-13 10:02:52 +0200

    [edit] js

--- js/script-dadi.js
+++ js/script-dadi.js
@@ -1,26 +1,18 @@
 
+const numeroPc = Math.round(Math.random()*5)+1;
 
-// Gioco dei dadi: generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
-
-//1. estraggo dei numeri casuali inserendoli in variabili;
-// 2. confronto i numeri 
-// 3. stabilisco vincitore
-
-
-let numeroPc = Math.round(Math.random()*5)+1;
-
-let numeroGiocatore = Math.round(Math.random()*5)+1;
+const numeroGiocatore = Math.round(Math.random()*5)+1;
 
 if (numeroPc > numeroGiocatore) {
   message = "ha vinto il computer!";
 
 } else if (numeroPc === numeroGiocatore) {
-  message = "quindi siete pari!";
+  message = "quindi avete pareggiato!";
   
 } else  message = "hai vinto tu!";
-
-
 
 document.getElementById("diceGame").innerHTML = " Dopo la verifica dell'email, è stato estratto un numero da 1 a 6, per te e per il computer: vince chi ha il numero più alto!";
 
 document.querySelector(".result").innerHTML =  "Per il pc è uscito il numero " + numeroPc + ", mentre per te il numero " + numeroGiocatore + " : " + message; 
+
+
--- js/script.js
+++ js/script.js
@@ -4,21 +4,18 @@ const listaMailsUtentiRegistrati = ["utente@hotmail.it", "ciao@yahoo.com", "ciao
 const mailUtente = prompt("Per verificare che tu sia registrato, inserisci il tuo indirizzo email");
 
 
-let utentePresente = false;
-
 let message = "la tua email " + mailUtente + " non risulta registrata";
 
 for (let i= 0; i < listaMailsUtentiRegistrati.length; i++) {
 
  
   if (listaMailsUtentiRegistrati[i] === mailUtente) {
-    utentePresente = true;
      message = "La tua email " + mailUtente + " risulta registrata";
   } 
 
   }
     
-
+  
 const display = document.querySelector("#output");
 display.innerHTML = message;
 
